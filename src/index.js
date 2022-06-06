7c8e0c516e1fac1cfeb61cf96efe452f167d5e06
Maksym Shumeiko <kkiilleerraa@mail.ru>
2022-06-06 10:33:52 +0300

    tese-comit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './css/styles.css';
-
+import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector("#search-box")
+input.addEventListener("input", debounce(onSearch,300))
+
+function onSearch(ev) {
+    ev.preventDefault();
+    const search = ev.target.value;
+    console.log(search)
+
+    fetch(`https://restcountries.com/v3.1/name/${search}`)
+    console.log(fetch(`https://restcountries.com/v3.1/name/${search}?fields=name,capital,population,languages,flags`))
+}
+
+// function fetchCountry(name) {
+    
+// }
